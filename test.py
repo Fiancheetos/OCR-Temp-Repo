dc360da88ae48000270e5d6ef4db38d6c57a0567
Ken Francisco <kdfrancisco2@up.edu.ph>
2025-06-17 13:54:42 +0800

    init: current progress

---
+++ test.py
@@ -0,0 +1,79 @@
+# Import necessary modules from FastAPI
+from fastapi import FastAPI, HTTPException, status
+from pydantic import BaseModel
+from typing import List, Optional
+import uvicorn
+
+from pdf2image import convert_from_path, convert_from_bytes
+from pdf2image.exceptions import (
+    PDFInfoNotInstalledError,
+    PDFPageCountError,
+    PDFSyntaxError
+)
+import pytesseract
+
+
+pytesseract.pytesseract.tesseract_cmd = r'/opt/homebrew/bin/tesseract'
+
+
+
+# Initialize the FastAPI application
+app = FastAPI(
+    title="Basic FastAPI Boilerplate",
+    description="A simple starting point for your FastAPI projects.",
+    version="0.1.0"
+)
+
+# --- Data Models (using Pydantic) ---
+# Pydantic models define the structure of your data (request bodies and response models).
+
+class Table(BaseModel):
+    """
+    Represents an item with a name and an optional description and price.
+    """
+    # overallGWA: float
+    recordTable: dict[str, List[str | float]]
+
+    class Config:
+        """
+        Configuration for the Pydantic model.
+        Used to provide example data for API documentation (OpenAPI/Swagger UI).
+        """
+        schema_extra = {
+            "example": {
+                # "overallGWA": 1.00,
+                "recordTable": {"HK 12": ["Human Kinetics", 1.00]}
+            }
+        }
+
+# --- PDF TO PNG CONVERSION ---
+
+def convert_to_png():
+    images = convert_from_path('/Users/fianchetto/Downloads/TORSample.pdf', output_folder='/Users/fianchetto/Desktop/Internship', fmt='png')
+    return images
+
+# --- OpenCV Preprocessing ---
+
+
+
+# --- Tesseract OCR ---
+
+def process_img(images):
+    for image in images:
+        print(pytesseract.image_to_string(image))
+        print("\n\n")
+
+# --- API Endpoints (Routes) ---
+
+@app.get("/api/")
+async def run_func():
+    images = convert_to_png()
+    process_img(images)
+    return {"message": "Sample Message"}
+
+# --- Run the application ---
+# This block allows you to run the FastAPI application directly using `python main.py`.
+# For production, you would typically run it using `uvicorn main:app --host 0.0.0.0 --port 8000`.
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
